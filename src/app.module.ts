918b24428d4416a77628c2312dee4b109dd0d425
anfedu <firdausnurilahmad@gmail.com>
2022-07-20 14:07:16 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+const dotenv = process.env;
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: dotenv.MYSQL_HOST,
+      port: parseInt(dotenv.MYSQL_PORT),
+      username: dotenv.MYSQL_USER,
+      password: dotenv.MYSQL_PASSWORD,
+      database: dotenv.MYSQL_DB,
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
