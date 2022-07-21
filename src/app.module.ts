be1385132fe6b76657743b5270140166903e7b43
anfedu <firdausnurilahmad@gmail.com>
2022-07-21 14:36:10 +0700

    generate user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
 
 const dotenv = process.env;
@@ -16,7 +17,7 @@ const dotenv = process.env;
       username: dotenv.MYSQL_USER,
       password: dotenv.MYSQL_PASSWORD,
       database: dotenv.MYSQL_DB,
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
     UserModule,
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  fullname: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
