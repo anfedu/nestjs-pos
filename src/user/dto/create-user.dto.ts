3b1f21036a5c5589f57f3eab5ce62593ce57022b
anfedu <firdausnurilahmad@gmail.com>
2022-07-21 19:35:20 +0700

    validation field

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsOptional()
+  id?: number;
+
+  @IsString()
+  @MaxLength(200)
+  fullname: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  username: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(30)
+  password: string;
+}
